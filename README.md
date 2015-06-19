32e039d7b11b26ccdacb14b2f4693f6867ba0631
thisisaaroland <aaron@mapzen.com>
2015-06-18 17:07:55 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# plumbing-vagrant
+
+Tools for managing Vagrant instances
+
+## Usage
+
+	$> ./build.sh foo
+	What port should we forward port 80 to: 
+	$> 9999
+	What port should we forward port 443 to: 
+	$> 6666
+	Shell hoohah on build: 
+	$> 
+	Spinning foo up now (y/n): 
+	$> n
+	all done
+
---
+++ build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+PERL=`which perl`
+PYTHON=`which python`
+
+WHOAMI=`${PYTHON} -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+WHEREAMI=`dirname $WHOAMI`
+
+PROJECT=`${PYTHON} -c 'import os, sys; print os.path.realpath(sys.argv[1])' $1`
+
+if [ -e ${PROJECT} ]
+then
+    echo "${PROJECT} already exists. I'm scared..."
+    exit 1
+fi
+
+mkdir -p ${PROJECT}
+
+cp ${WHEREAMI}/Vagrantfile.template ${PROJECT}/Vagrantfile
+cp ${WHEREAMI}/Makefile.template ${PROJECT}/Makefile
+
+NAME=`basename ${PROJECT}`
+
+${PERL} -pi -e "s!__VAGRANT_HOST_22__!${PORT}!" ${PROJECT}/Vagrantfile
+
+echo "What port should we forward port 80 to: "
+read PORT
+
+${PERL} -pi -e "s!__VAGRANT_HOST_80__!${PORT}!" ${PROJECT}/Vagrantfile
+
+echo "What port should we forward port 443 to: "
+read PORT
+
+${PERL} -pi -e "s!__VAGRANT_HOST_443__!${PORT}!" ${PROJECT}/Vagrantfile
+
+echo "Shell hoohah on build: "
+read SHELL
+
+if [ "${SHELL}" != "" ]
+then
+    if [ -e ${SHELL} ]
+    then
+	echo "reading data for shell hoohah from ${SHELL}"
+	SHELL=`cat ${SHELL}`
+    fi
+fi
+
+${PERL} -pi -e "s!__VAGRANT_SHELL__!${SHELL}!" ${PROJECT}/Vagrantfile
+
+echo "Spinning ${NAME} up now (y/n): "
+read UP
+
+if [ "${UP}" == "y" ]
+then
+
+    cd ${PROJECT}
+    echo vagrant up
+    cd -
+fi
+
+echo "all done"
+exit 0
